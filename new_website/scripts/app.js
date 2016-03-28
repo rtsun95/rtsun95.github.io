a6811f8e6b597aa8ca7c7e6fa7a6e57936ebd59c
rtsun95 <sunruotai95@gmail.com>
2016-03-27 21:39:08 -0400

    init new website

---
+++ new_website/scripts/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Content from "./components/Content";
+
+const app = document.getElementById('app');
+ReactDOM.render(<Content />, app);
