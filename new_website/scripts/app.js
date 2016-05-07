b1737b14ebe7b0f733e230ae67fbb202152e3c4c
rtsun95 <sunruotai95@gmail.com>
2016-05-07 17:49:20 -0400

    use browser history

--- new_website/scripts/app.js
+++ new_website/scripts/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, hashHistory} from 'react-router';
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import Content from "./components/Content";
 import About from './components/About';
@@ -9,7 +9,7 @@ import Project from './components/Project';
 
 const app = document.getElementById('app');
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path='/' component={Content}>
       <IndexRoute component={About}></IndexRoute>
       <Route path='experiences' component={Experience}></Route>
